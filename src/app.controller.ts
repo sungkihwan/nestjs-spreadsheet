369b2c873089baaf0b10a3e9f5bf58f514816602
kihwan <skh@companylabs.co.kr>
2023-08-06 19:34:49 +0900

    transform data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,42 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SpreadsheetService } from '@libs/spreadsheet';
+
+interface KeyData {
+  [key: string]: any;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly spreadsheetService: SpreadsheetService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/test')
+  async test(): Promise<any> {
+    const result = await this.spreadsheetService.getValuesByMultiEditor(
+      '1KK36IHmo5vKZBQ72ZA_hAW_9mgpjrkzzwvnjTvdZ-jE',
+      '시트1!A1:W1000',
+    );
+    const transformedData = this.transform(result);
+    console.log(transformedData);
+  }
+
+  private transform(data: string[][]): KeyData[] {
+    const years = data[0];
+    const rows = data.slice(1);
+
+    return rows.map((row) => {
+      return row.reduce((obj: KeyData, value: string, index: number) => {
+        obj[years[index]] = value;
+        return obj;
+      }, {});
+    });
   }
 }
